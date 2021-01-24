f11f1b203b4986e0ba96b1218d0d6809b40c5d06
Daniel Desmond <danieldesmond@daniels-air.lan>
2021-01-24 13:40:56 -0500

    created insult generator and sends to api

--- src/CrtAcc/crtAcc.js
+++ src/CrtAcc/crtAcc.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import './crtAcc.css';
 
+const adjectives = ["idiotic", "stupid", "ugly", "dumb", "weird", "nerdy", "lazy", "boring", "lame", "illiterate"];
+const nouns = ["log", "dog", "brick", "chair", "mound of dirt", "rock", "tree stump", "idiot", "nerd", "tool"];
+
 class CrtAcc extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             username: "",
-            password: ""
+            password: "",
+            created: "",
+            insult: this.insultGenarator(),
         }
-        this.thing  = this.thing.bind(this);
         this.handleUsernameChange = this.handleUsernameChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.sendToApi = this.sendToApi.bind(this);
+        this.insultGenarator = this.insultGenarator.bind(this);
     }
-    thing = () => {return "foo";};
+
+    insultGenarator() {
+        let adj = adjectives[Math.floor(Math.random() * adjectives.length)];
+        let noun = nouns[Math.floor(Math.random() * nouns.length)];
+        return `You are a ${adj} ${noun}`
+    }
 
     handleUsernameChange(event) {
         this.setState({ username: event.target.value });
@@ -20,6 +32,22 @@ class CrtAcc extends React.Component {
 
       handlePasswordChange(event) {
         this.setState({ password: event.target.value });
+      }
+
+      handleClick() {
+          if (this.state.username !== "" && this.state.password !== ""){
+              this.setState({ created: `Created!, your insult is '${this.state.insult}'`, username: "", password: "" });
+              this.sendToApi();
+          } else {
+              this.setState({ created: "Both fields must be filled in in order to process request" })
+          }
+      }
+
+      sendToApi() {
+        let username = this.state.username;
+        let password = this.state.password;
+        let insult = this.state.insult;
+        fetch(`http://localhost:9000/crtacc?username=${username}&password=${password}&phrase=${insult}`)
       }
 
     render() {
@@ -34,6 +62,9 @@ class CrtAcc extends React.Component {
                     <br />
                     <input type="text" onChange={this.handlePasswordChange} value={this.state.password} placeholder="Password here" />
                 </form>
+                <button onClick={this.handleClick} >Confirm</button>
+                <br />
+                {this.state.created}
             </div>
         )
     }
