5f23313a071c7245d72f3ff438eced43d18ee7e8
Daniel Desmond <danieldesmond@daniels-air.lan>
2021-01-24 17:42:13 -0500

    added logic to prevent double usernames

--- api/routes/crtacc.js
+++ api/routes/crtacc.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var databse = require("./database");
+const database = require('./database');
 
 router.get("/", (req, res, next) => {
+    for (let user of database) {
+        if (user.username === req.query.username) {
+            res.status(400).send("Username is already registered")
+        }
+    }
     res.status(200).send("success");
     console.log(databse);
     console.log("Heyy");
--- api/routes/database.js
+++ api/routes/database.js
@@ -1,3 +1,9 @@
-let database = [];
+let database = [
+    {
+        username: "dezy",
+        password: "dezy43",
+        phrase: "You are a nerdy tree stump"
+    }
+];
 
 module.exports = database;
